46893789853d4d854c91b44bd7ae3c44949981eb
gregorytrentmartinjr <110754385+gregorytrentmartinjr@users.noreply.github.com>
2026-02-16 21:57:17 -0600

    Add illogical-impulse-extras

--- sdata/dist-arch/uninstall-deps.sh
+++ sdata/dist-arch/uninstall-deps.sh
@@ -1,6 +1,6 @@
 # This script is meant to be sourced.
 # It's not for directly running.
 
-for i in illogical-impulse-{quickshell-git,audio,backlight,basic,bibata-modern-classic-bin,fonts-themes,gnome,hyprland,kde,microtex-git,oneui4-icons-git,portal,python,screencapture,toolkit,widgets} plasma-browser-integration; do
+for i in illogical-impulse-{quickshell-git,audio,backlight,basic,bibata-modern-classic-bin,extras,fonts-themes,gnome,hyprland,kde,microtex-git,oneui4-icons-git,portal,python,screencapture,toolkit,widgets} plasma-browser-integration; do
   v yay -Rns $i
 done
